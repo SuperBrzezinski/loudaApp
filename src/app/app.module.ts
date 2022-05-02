a03e0c3326200403020728d95ae6c99109de390a
SuperBrzezinski <michal.a.brzezinski@gmail.com>
2022-05-02 17:40:27 +0200

    feat: basic authentication was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -11,13 +13,26 @@ import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { environment } from '../environments/environment';
+
 import { TestcomponentComponent } from './testcomponent/testcomponent.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { AuthService } from './shared/services/auth.service';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [AppComponent, TestcomponentComponent],
+  declarations: [
+    AppComponent,
+    TestcomponentComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+  ],
   imports: [
     BrowserModule,
     NgbModule,
+    RouterModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
@@ -25,7 +40,7 @@ import { TestcomponentComponent } from './testcomponent/testcomponent.component'
     AngularFireStorageModule,
     AngularFireDatabaseModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
